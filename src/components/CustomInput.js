2e37ce4e4ba81b5afe4c622d69a302f2ba91bb14
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-06-27 21:23:20 +0545

    custom input

---
+++ src/components/CustomInput.js
@@ -0,0 +1,15 @@
+const CustomInput = ({ label, i_class, i_id, type }) => {
+  return (
+    <div className="form-floating mb-3">
+      <input
+        type={type}
+        className={`form-control + ${i_class}`}
+        id={i_id}
+        placeholder={label}
+      />
+      <label htmlFor={label}>{label}</label>
+    </div>
+  );
+};
+
+export default CustomInput;
