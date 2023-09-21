77b971b3b24d98c1bf2e44c0bdef40675c90de33
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-09-21 14:39:53 +0545

    integrate color api

--- src/pages/Colorlist.js
+++ src/pages/Colorlist.js
@@ -1,5 +1,18 @@
 import { Table } from "antd";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getColors } from "../features/color/colorSlice";
+import { Link } from "react-router-dom";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
 const Colorlist = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getColors());
+  }, []);
+  const colorState = useSelector((state) => state.color);
+  const { colors } = colorState;
+
   const columns = [
     {
       title: "SNo",
@@ -10,21 +23,25 @@ const Colorlist = () => {
       dataIndex: "name",
     },
     {
-      title: "Product",
-      dataIndex: "product",
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
+      title: "Action",
+      dataIndex: "action",
     },
   ];
   const data1 = [];
-  for (let i = 0; i < 46; i++) {
+  for (let i = 0; i < colors.length; i++) {
     data1.push({
       key: i,
-      name: `Edward King ${i}`,
-      product: 32,
-      status: `London, Park Lane no. ${i}`,
+      name: colors[i].title,
+      action: (
+        <>
+          <Link className="fs-4 text-danger" to="/">
+            <BiEdit />
+          </Link>
+          <Link className="ms-2 fs-4 text-danger" to="/">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
     });
   }
   return (
