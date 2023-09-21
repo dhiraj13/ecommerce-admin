e0c10179aba78dbf1c1266e8b41721f3130858c3
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-09-21 12:48:04 +0545

    integrate api for customer and product

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,5 +1,16 @@
 import { Table } from "antd";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../features/customers/customerSlice";
+
 const Customers = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUsers());
+  }, []);
+  const customerstate = useSelector((state) => state.customer);
+  const { customers } = customerstate;
+
   const columns = [
     {
       title: "SNo",
@@ -8,25 +19,29 @@ const Customers = () => {
     {
       title: "Name",
       dataIndex: "name",
+      sorter: (a, b) => a.name.length - b.name.length,
     },
     {
-      title: "Product",
-      dataIndex: "product",
+      title: "Email",
+      dataIndex: "email",
     },
     {
-      title: "Status",
-      dataIndex: "status",
+      title: "Phone Number",
+      dataIndex: "mobile",
     },
   ];
   const data1 = [];
-  for (let i = 0; i < 46; i++) {
-    data1.push({
-      key: i,
-      name: `Edward King ${i}`,
-      product: 32,
-      status: `London, Park Lane no. ${i}`,
-    });
+  for (let i = 0; i < customers.length; i++) {
+    if (customers[i].role !== "admin") {
+      data1.push({
+        key: i,
+        name: `${customers[i].firstname} ${customers[i].lastname}`,
+        email: customers[i].email,
+        mobile: customers[i].mobile,
+      });
+    }
   }
+
   return (
     <div>
       <h3 className="mb-4 title">Customers</h3>
--- src/pages/Productlist.js
+++ src/pages/Productlist.js
@@ -1,33 +1,71 @@
 import { Table } from "antd";
+import { useEffect } from "react";
 import { BiEdit } from "react-icons/bi";
+import { Link } from "react-router-dom";
 import { AiFillDelete } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../features/product/productSlice";
 
 const Productlist = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+  const productState = useSelector((state) => state.product);
+  const { products } = productState;
   const columns = [
     {
       title: "SNo",
       dataIndex: "key",
     },
     {
-      title: "Name",
-      dataIndex: "name",
+      title: "Title",
+      dataIndex: "title",
+      sorter: (a, b) => a.title.length - b.title.length,
     },
     {
-      title: "Product",
-      dataIndex: "product",
+      title: "Brand",
+      dataIndex: "brand",
+      sorter: (a, b) => a.brand.length - b.brand.length,
     },
     {
-      title: "Status",
-      dataIndex: "status",
+      title: "Category",
+      dataIndex: "category",
+      sorter: (a, b) => a.category.length - b.category.length,
+    },
+    {
+      title: "Color",
+      dataIndex: "color",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      sorter: (a, b) => a.price.length - b.price.length,
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
     },
   ];
   const data1 = [];
-  for (let i = 0; i < 46; i++) {
+  for (let i = 0; i < products.length; i++) {
     data1.push({
       key: i,
-      name: `Edward King ${i}`,
-      product: 32,
-      status: `London, Park Lane no. ${i}`,
+      title: products[i].title,
+      brand: products[i].brand,
+      category: products[i].category,
+      color: products[i].color,
+      price: `$ ${products[i].price}`,
+      action: (
+        <>
+          <Link className="fs-4 text-danger" to="/">
+            <BiEdit />
+          </Link>
+          <Link className="ms-2 fs-4 text-danger" to="/">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
     });
   }
   return (
